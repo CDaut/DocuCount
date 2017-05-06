e3a5cd8fa7922e5fb72ee781fe19a42263268c14
Unknown <clemens.dautermann@t-online.de>
2017-05-06 21:37:07 +0200

    added ability to count words

--- PowerPoint/counter.py
+++ PowerPoint/counter.py
@@ -9,7 +9,7 @@ def count(p):
 
     #get all text
     for slide in p.slides: #each slide
-        for shape in slides.shape:  #each shape
+        for shape in slide.shapes:  #each shape
             if(not shape.has_text_frame): #continue if the shape has no text frames
                 continue
             for paragraph in shape.text_frame.paragraphs: #for each paragraph
@@ -39,10 +39,42 @@ while(pres == "null"):
         #get presentation
         pres = Presentation(filename)
 
+        #count file
         text = count(pres)
-        print(text)
+
+        #create list to store all words
+        allWords = []
+        allWordsTemp = []
+
+        #each text line
+        for n in range(len(text)):
+
+            tempLine = text[n]
+
+            #each word in tempLine
+            for i in range(len(tempLine.split(" "))):
+
+                #add each word
+                tempWordsSplit = tempLine.split(" ")
+                allWordsTemp.append(tempWordsSplit[i])
+
+
+
+        #remove word if its just a space
+        for n in range(len(allWordsTemp)):
+
+            #if the word is a space it may not be counted
+            if(allWordsTemp[n] == "" or allWordsTemp[n] == " "):
+                    pass
+
+            else:
+                allWords.append(allWordsTemp[n])
+
+        print("-------------------------------------------")
+        print("Total words: " + str(len(allWords)))
 
     except Exception as exc:
 
         print("File not supported!")
         pres = "null"
+        print(exc)
